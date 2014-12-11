326593bc5f31072b945f5c6c068a14aa810eeaa2
Jessie Xiong <jessiex@synnex.com>
2014-12-11 18:13:31 +0800

    Add arguments to the decorator.

--- scripts/decorator_track.py
+++ scripts/decorator_track.py
@@ -3,26 +3,42 @@ from functools import wraps
 import sys,types
 n = 0
 indent = 0
-import_name = 'decorate.py'
-def print_name(func):
+import_name = 'decorate_.py'
+list = []
+def print_name(print_arg=True, print_arg_once=True, print_once=False):
     """ general decorator"""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+    def decorate(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            global n, last_func,indent
+            indent+=1
+            simple_item = func.__module__+'.'+func.__name__
+            if print_arg:
+                new_item= func.__module__+'.'+func.__name__+':'+str(args)[:100]+' '+str(kwargs)[:100]
+                if print_arg_once:
+                    if simple_item in list:
+                        new_item = simple_item
+                    else:
+                        list.append(simple_item)
+            else:
+                new_item = simple_item
+            if print_once and new_item in list:
+                pass
+            else:
+                print '*'*indent,'Calling',new_item
+                list.append(new_item)
+            #this will print only if there is exception
+            #f = sys.exc_info()[2].tb_frame.f_back
+            #print f.f_code
+            result = func(*args, **kwargs)
+            #print '*'*indent,'Ret of  %s: ' % (func.__module__+'.'+func.__name__), result
+            if indent == 1:
+                print '\n'
+            indent -=1
+            return result
+        return wrapper
+    return decorate
 
-        global n, last_func,indent
-        indent+=1
-        new_item= func.__module__+'.'+func.__name__
-        print '*'*indent,'Calling',new_item
-        #this will print only if there is exception
-        #f = sys.exc_info()[2].tb_frame.f_back
-        #print f.f_code
-        result = func(*args, **kwargs)
-        #print '*'*indent,'Ret of  %s: ' % (func.__module__+'.'+func.__name__), result
-        if indent == 1:
-            print '\n'
-        indent -=1
-        return result
-    return wrapper
 import os,re,shutil
 def remove_decorator(source_dir=None):
     # source_dir='/home/jessiex/git/stress/scripts'
@@ -36,8 +52,7 @@ def remove_decorator(source_dir=None):
             if file.endswith('.d_bak'):
                 shutil.copy(filepath,filepath[:-1*len('.d_bak')])
 
-
-def add_decorator(source_dir=None):
+def add_decorator(source_dir=None, print_arg=True, print_arg_once=True, print_once=False):
     # source_dir='/home/jessiex/git/stress/scripts'
     if source_dir == None:
         source_dir = os.getcwd()
@@ -45,12 +60,10 @@ def add_decorator(source_dir=None):
 
         print (top, folders, files)
         for file in files:
-
             filepath = os.path.join(top,file)
             shutil.copy(filepath,filepath+'.d_bak')
             if filepath == __file__ or file == 'xml2obj.py':
                 continue
-
             if file.endswith('.py') == False or os.path.isdir(filepath) or file==import_name:
                 continue
             r = open(filepath)
@@ -78,7 +91,8 @@ def add_decorator(source_dir=None):
                             import_added = True
                     file_index = content[history_index:].find(line)
                     file_index = file_index + history_index
-                    decorator_line += indent+'@print_name\n'
+                    decorator_line += indent+'@print_name(print_arg=%s, print_arg_once=%s, print_once=%s)\n' % \
+                                             (str(print_arg), str(print_arg_once), str(print_once))
                     content = content[:file_index]+decorator_line+content[file_index:]
                     history_index = file_index
 
@@ -93,4 +107,6 @@ if __name__ == '__main__':
     print __file__
     if os.path.basename(__file__) != import_name:
         shutil.copy(__file__, import_name)
-        add_decorator(os.getcwd())
+        add_decorator(os.getcwd(),print_arg=True, print_arg_once=True, print_once=False)
+
+
